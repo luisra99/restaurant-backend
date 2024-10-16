70b301fb358d85851bd8951f8ce819f704d5d9c2
luisra99 <luisraul.alfonsoc@gmail.com>
2024-10-16 03:34:45 -0400

    update

--- src/controller/account.controller.ts
+++ src/controller/account.controller.ts
@@ -39,18 +39,94 @@ export const openAccount = async (req: Request, res: Response) => {
     res.status(500).json(descripcionError);
   }
 };
-
 // Listar todos los Conceptos
-export const listConcepts = async (req: Request, res: Response) => {
+export const getAccount = async (req: Request, res: Response) => {
   try {
-    const concepts = await prisma.concept.findMany({
+    const { id } = req.params;
+
+    const account = await prisma.account.findFirst({
+      where: { id: Number(id) },
       include: {
-        conceptFather: true,
-        childConcept: true,
+        table: true,
+        details: { include: { offer: true } },
+        type: true,
+      },
+    });
+    const orders = account?.details.map((detail) => {
+      return {
+        id: detail.offer.id,
+        name: detail.offer.name,
+        quantity: detail.quantity,
+        totalPrice: detail.quantity * Number(detail.offer.price), // Multiplica cantidad por precio
+      };
+    });
+    // Calcular el total de todas las cantidades
+    const totalQuantity = orders?.reduce(
+      (sum, order) => sum + order.quantity,
+      0
+    );
+    // Calcular el total de todas las cantidades
+    // Calcular el precio total de todas las ofertas
+    const totalPrice =
+      orders?.reduce((sum, order) => sum + order.totalPrice, 0) ?? 0;
+
+    const taxsDiscounts = account?.taxDiscount.length
+      ? await prisma.taxDiscounts.findMany({
+          where: {
+            id: { in: account.taxDiscount },
+          },
+        })
+      : [];
+    // Calcular el precio final basado en los impuestos y descuentos
+    let finalPrice = totalPrice;
+    const mappedTaxsDiscounts = taxsDiscounts.map((item: any) => {
+      const amount = (item.percent / 100) * totalPrice;
+
+      // Si es impuesto, lo sumamos al precio final
+      if (item.tax) {
+        finalPrice += amount;
+      } else {
+        // Si es descuento, lo restamos del precio final
+        finalPrice -= amount;
+      }
+
+      // Devolvemos el objeto con el monto calculado
+      return {
+        ...item,
+        amount, // Monto calculado basado en el percent
+      };
+    });
+    res.status(200).json({
+      ...account,
+      mappedTaxsDiscounts,
+      orders,
+      totalQuantity,
+      totalPrice,
+      finalPrice,
+    });
+  } catch (error) {
+    const err = error as Error & { code?: string };
+
+    const descripcionError = {
+      message: "Ha ocurrido un error listando los conceptos.",
+      code: err.code || "SERVER_ERROR",
+      stackTrace: err.stack || "NO_STACK_TRACE_AVAILABLE",
+    };
+
+    res.status(500).json(descripcionError);
+  }
+};
+// Listar todas las cuentas
+export const listAccounts = async (req: Request, res: Response) => {
+  try {
+    const accounts = await prisma.account.findMany({
+      where: { active: true },
+      include: {
+        table: true,
       },
     });
 
-    res.status(200).json(concepts);
+    res.status(200).json(accounts);
   } catch (error) {
     const err = error as Error & { code?: string };
 
--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import {
   openAccount,
-  listConcepts,
+  listAccounts,
   updateConcept,
   deleteConcept,
   findConceptsByFather,
   getConcept,
+  getAccount,
 } from "../controller/account.controller";
 
 const router = Router();
 
 router.post("/accounts", openAccount);
+router.get("/accounts", listAccounts);
+router.get("/accounts/:id", getAccount);
 
 export default router;
