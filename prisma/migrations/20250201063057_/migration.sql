11584082209b0d72822dc4fa5b82c274ec389cac
luisra99 <luisraul.alfonsoc@gmail.com>
2025-02-01 08:46:09 -0500

    delete migrations

--- prisma/migrations/20250201063057_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "InventoryMovement" ADD COLUMN     "stockInputMovementId" TEXT;
--- prisma/migrations/20250201063647_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "InventoryMovement" ADD CONSTRAINT "InventoryMovement_stockInputMovementId_fkey" FOREIGN KEY ("stockInputMovementId") REFERENCES "InventoryMovement"("id") ON DELETE SET NULL ON UPDATE CASCADE;
